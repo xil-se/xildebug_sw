0c353b565f27bf469133c2c1d31096a38cd8d203
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-04-19 20:29:23 +0200

    gpio: Expose LED_RGB_R_Pin

--- app/drivers/gpio.c
+++ app/drivers/gpio.c
@@ -13,7 +13,7 @@ void gpio_init(void)
 
 	HAL_GPIO_WritePin(GPIOC, GPIO2_Pin | GPIO3_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(GPIOH, DUT_UART_TX_LED_Pin | DUT_UART_RX_LED_Pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, DUT_SWD_LED_Pin | SHUNT1_EN_Pin | SHUNT2_EN_Pin | LED_RGB_G_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, DUT_SWD_LED_Pin | SHUNT1_EN_Pin | SHUNT2_EN_Pin | LED_RGB_R_Pin | LED_RGB_G_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(GPIOB, DUT_VDD_EN_Pin | DUT_SWCLK_Pin | DUT_SWDIO_Pin | LED_RGB_B_Pin | GPIO0_Pin | GPIO1_Pin, GPIO_PIN_RESET);
 
 	gpio_config.Pin = GPIO2_Pin | GPIO3_Pin;
@@ -38,7 +38,7 @@ void gpio_init(void)
 	gpio_config.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOA, &gpio_config);
 
-	gpio_config.Pin = DUT_SWD_LED_Pin | SHUNT1_EN_Pin | SHUNT2_EN_Pin | LED_RGB_G_Pin;
+	gpio_config.Pin = DUT_SWD_LED_Pin | SHUNT1_EN_Pin | SHUNT2_EN_Pin | LED_RGB_R_Pin | LED_RGB_G_Pin;
 	gpio_config.Mode = GPIO_MODE_OUTPUT_PP;
 	gpio_config.Pull = GPIO_NOPULL;
 	gpio_config.Speed = GPIO_SPEED_FREQ_LOW;
