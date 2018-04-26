0c51f280074bb56690dcceda7b063de07c6e9539
arturo182 <arturo182@tlen.pl>
2018-04-26 22:15:46 +0200

    mcp4018t: Cleanup the module

--- app/drivers/mcp4018t.c
+++ app/drivers/mcp4018t.c
@@ -1,28 +1,42 @@
 #include "drivers/mcp4018t.h"
 #include "drivers/i2c.h"
 
+#include <stdbool.h>
+
 #define MCP4018T_ADDRESS	0x2F
-#define I2C_TIMEOUT			100
+#define I2C_TIMEOUT_MS		100
 #define SELF_TEST_VALUE		0x2A
 #define DEFAULT_VALUE		0x01
 #define MAX_VALUE			0x7f
 
-static uint8_t mcp4018t_value;
+static struct {
+	bool initialized;
+	uint8_t value;
+} self;
 
 err_t mcp4018t_set_value(uint8_t val)
 {
+	if (!self.initialized)
+		return EMCP4018T_NO_INIT;
+
 	if (val > MAX_VALUE)
 		return EMCP4018T_INVALID_ARG;
 
-	if (mcp4018t_value == val)
+	if (self.value == val)
 		return ERR_OK;
 
-	return i2c_master_tx(MCP4018T_ADDRESS << 1, &val, sizeof(val), I2C_TIMEOUT);
+	return i2c_master_tx(MCP4018T_ADDRESS << 1, &val, sizeof(val), I2C_TIMEOUT_MS);
 }
 
 err_t mcp4018t_get_value(uint8_t *p_val)
 {
-	return i2c_master_rx(MCP4018T_ADDRESS << 1, p_val, sizeof(*p_val), I2C_TIMEOUT);
+	if (!self.initialized)
+		return EMCP4018T_NO_INIT;
+
+	if (!p_val)
+		return EMCP4018T_INVALID_ARG;
+
+	return i2c_master_rx(MCP4018T_ADDRESS << 1, p_val, sizeof(*p_val), I2C_TIMEOUT_MS);
 }
 
 err_t mcp4018t_init(void)
@@ -30,19 +44,35 @@ err_t mcp4018t_init(void)
 	uint8_t value;
 	err_t r;
 
+	if (self.initialized)
+		return ERR_OK;
+
+	self.initialized = true;
+
 	r = mcp4018t_set_value(SELF_TEST_VALUE);
-	ERR_CHECK(r);
+	if (r != ERR_OK) {
+		self.initialized = false;
+		return r;
+	}
 
 	r = mcp4018t_get_value(&value);
-	ERR_CHECK(r);
+	if (r != ERR_OK) {
+		self.initialized = false;
+		return r;
+	}
 
-	if (value != SELF_TEST_VALUE)
+	if (value != SELF_TEST_VALUE) {
+		self.initialized = false;
 		return EMCP4018T_INVALID_RESPONSE;
+	}
 
 	r = mcp4018t_set_value(DEFAULT_VALUE);
-	ERR_CHECK(r);
+	if (r != ERR_OK) {
+		self.initialized = false;
+		return r;
+	}
 
-	mcp4018t_value = DEFAULT_VALUE;
+	self.value = DEFAULT_VALUE;
 
-	return r;
+	return ERR_OK;
 }
--- app/drivers/mcp4018t.h
+++ app/drivers/mcp4018t.h
@@ -5,6 +5,7 @@
 
 #define EMCP4018T_INVALID_RESPONSE	(EMCP4018T_BASE + 0)
 #define EMCP4018T_INVALID_ARG		(EMCP4018T_BASE + 1)
+#define EMCP4018T_NO_INIT			(EMCP4018T_BASE + 2)
 
 err_t mcp4018t_set_value(uint8_t val);
 err_t mcp4018t_get_value(uint8_t *p_val);
