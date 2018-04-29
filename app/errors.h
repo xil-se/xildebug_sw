3f39b2763b3693f140b8dee71c6e4a90766a55d3
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-04-29 22:58:49 +0200

    errors: Add get/set macro for extracting HAL errors

--- app/errors.h
+++ app/errors.h
@@ -10,10 +10,13 @@ typedef uint32_t err_t;
 			for(;;); \
 	} while (0)
 
+#define HAL_ERROR_GET(x) (((x) >> 14) & 0x3)
+#define HAL_ERROR_SET(hal_status, err_to_return) (err_to_return | ((hal_status) << 14))
+
 #define HAL_ERR_CHECK(hal_status, err_to_return) \
 	do { \
 		if ((hal_status) != HAL_OK) \
-			return (err_to_return | ((hal_status) << 14)); \
+			return HAL_ERROR_SET(err_to_return, hal_status); \
 	} while (0)
 
 #define ERR_OK			(0)
